44c37ede0d1667666b7500c652942212f04416d0
Sep-Binkhorst <sepbinkhorst@gmail.com>
2024-11-01 13:40:09 +0100

    Eerste opdracht

--- basisscript.py
+++ basisscript.py
@@ -5,7 +5,19 @@ rm = pyvisa.ResourceManager("@py")
 device = rm.open_resource(
     "ASRL8::INSTR", read_termination="\r\n", write_termination="\n")
 
-device.query(f"OUT:CH0 1023")
+for voltage in range(0, 1023):
+    device.query(f"OUT:CH0 {voltage}")
+    raw_u2 = device.query(f"MEAS:CH2?")
+    print(f"De raw voltage op de weerstand is {raw_u2}")
+    voltage_u2 = float(raw_u2) * (3.3/1024)
+    print(f"De voltage op de weerstand is {voltage_u2}")
+    raw_u1 = device.query(f"MEAS:CH1?")
+    voltage_u1 = float(raw_u1) * (3.3/1024)
 
-device.query(f"MEAS:CH1")
-device.query(f"MEAS:CH2")
+    raw_led = float(raw_u1) - float(raw_u2)
+    voltage_led = voltage_u1 - voltage_u2
+    print(f"De raw voltage op de led is {raw_led}")
+    print(f"De voltage op de led is {voltage_led}")
+
+
+
