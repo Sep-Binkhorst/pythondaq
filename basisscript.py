ddeedee78e652916cdd049a07a47c68bd9ad6915
Sep-Binkhorst <sepbinkhorst@gmail.com>
2024-11-05 10:35:23 +0100

    Code van de class verplaatst naar een ander script om te gebruiken in basisscript.py.

--- basisscript.py
+++ basisscript.py
@@ -1,64 +1,8 @@
-import pyvisa
+from arduino_device import ArduinoVISADevice, list_resources
 import matplotlib.pyplot as plt
-import time
-import csv
-
-rm = pyvisa.ResourceManager("@py")
-
-def list_resources():
-    ports = rm.list_resources()
-    return ports
-
-class ArduinoVISADevice:
-    def __init__(self, port):
-        self.device = rm.open_resource(f"{port}", read_termination="\r\n", write_termination="\n")
-
-    def get_identification(self):
-        identification = self.device.query("*IDN?")
-        return identification
-    
-    def set_output_value(self, value):
-        self.device.query(f"OUT:CH0 {value}")
-
-    def get_output_value(self):
-        output = self.device.query(f"MEAS:CH0?")
-        return output
-
-    def get_input_value(self, channel):
-        raw = self.device.query(f"MEAS:CH{channel}?")
-        return raw
-
-    def get_input_voltage(self, channel):
-        raw = self.device.query(f"MEAS:CH{channel}?")
-        voltage = float(raw) * (3.3/1024)
-        return voltage
-
-
-# #Controller bouwen opdrachten
-# print(list_resources())
 
 # Create an instance for the Arduino on port "ASRL28::INSTR"
 device = ArduinoVISADevice(port="ASRL8::INSTR")
-
-# # print identification string
-# identification = device.get_identification()
-# print(identification)
-
-# # set OUTPUT voltage on channel 0, using ADC values (0 - 1023)
-# device.set_output_value(value=828)
-
-# # measure the voltage on INPUT channel 2 in ADC values (0 - 1023)
-# ch2_value = device.get_input_value(channel=2)
-# print(f"{ch2_value=}")
-
-# # measure the voltage on INPUT channel 2 in volts (0 - 3.3 V)
-# ch2_voltage = device.get_input_voltage(channel=2)
-# print(f"{ch2_voltage=}")
-
-# # get the previously set OUTPUT voltage in ADC values (0 - 1023)
-# ch0_value = device.get_output_value()
-# print(f"{ch0_value=}")
-
 
 #Controller Implementeren Opdracht
 voltages = []
@@ -80,6 +24,3 @@ plt.plot(voltages, currents, 'o')
 plt.xlabel('Voltage (V)')
 plt.ylabel('Current (A)')
 plt.show()
-
-#Controller Afsplitsen Opdracht
-
