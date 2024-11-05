e81a579617e8a1ac749711ad257d004acbb3c0fc
Sep-Binkhorst <sepbinkhorst@gmail.com>
2024-11-05 12:34:40 +0100

    Bezig met het schrijven van de code om de errors te berekenen.

--- diode_experiment.py
+++ diode_experiment.py
@@ -1,25 +1,58 @@
 from arduino_device import ArduinoVISADevice, list_resources
+import math
 
 print(list_resources())
 
 class DiodeExperiment:
 
-    def scan(start, stop):
+    def scan(start, stop, repeats):
         device = ArduinoVISADevice(port="ASRL8::INSTR")
 
-        voltages = []
-        currents = []
+        voltage_avrs = []
+        current_avrs = []
+        vol_dev_avrs = []
+        cur_dev_avrs = []
+        for repeat in range(1, repeats):
 
-        for voltage in range(start, stop):
-            device.set_output_value(value=voltage)
+            voltages = []
+            voltage_count = 0
 
-            voltage_u1 = device.get_input_voltage(channel=1)
-            voltage_u2 = device.get_input_voltage(channel=2)
+            currents = []
+            current_count = 0
 
-            voltage_led = voltage_u1 - voltage_u2
-            voltages.append(voltage_led)
+            for voltage in range(start, stop):
+                device.set_output_value(value=voltage)
 
-            current_led = voltage_u2 / 220
-            currents.append(current_led)
+                voltage_u1 = device.get_input_voltage(channel=1)
+                voltage_u2 = device.get_input_voltage(channel=2)
+
+                voltage_led = voltage_u1 - voltage_u2
+                voltages.append(voltage_led)
+
+                current_led = voltage_u2 / 220
+                currents.append(current_led)
+
+                voltage_count += 1
+                current_count += 1
+
+            for voltage, voltage_avrs in zip(voltages, voltage_avrs):
+                vol_dev = (((voltage - voltage_avrs))/len(voltages))**0.5
+                vol_dev_avrs.append(vol_dev/(repeats**0.5))
+
+            for current, current_avrs in zip(currents, current_avrs):
+                cur_dev = (((current - current_avrs))/len(currents))**0.5
+                cur_dev_avrs.append(cur_dev/(repeats**0.5))
+
+            voltage_avr = sum(voltages) / voltage_count
+            current_avr = sum(currents) / current_count
+            
+            voltage_avrs.append(voltage_avr)
+            current_avrs.append(current_avr)
+
+
+        return voltage_avrs, current_avrs, vol_dev_avrs, cur_dev_avrs
+
+
         
-        return voltages, currents
+            
+            
--- view.py
+++ view.py
@@ -1,10 +1,11 @@
 from diode_experiment import DiodeExperiment
 import matplotlib.pyplot as plt
 
-voltages, currents = DiodeExperiment.scan(start=0, stop=1023)
+voltages, currents, v_errors, c_errors = DiodeExperiment.scan(start=0, stop=1023, repeats = 3)
 
 plt.figure()
 plt.plot(voltages, currents, 'o')
+plt.errorbar(v_errors, c_errors)
 plt.xlabel('Voltage (V)')
 plt.ylabel('Current (A)')
 plt.show()
