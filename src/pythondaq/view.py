a71a71e1acdcc55a54d846930481a82ed1d1541e
Sep-Binkhorst <sepbinkhorst@gmail.com>
2024-11-12 12:41:12 +0100

    Commando via poetry werkt.

--- src/pythondaq/view.py
+++ src/pythondaq/view.py
@@ -1,11 +1,14 @@
 from pythondaq.diode_experiment import DiodeExperiment
 import matplotlib.pyplot as plt
 
-diodeexperiment = DiodeExperiment()
-voltages, currents, v_errors, c_errors = diodeexperiment.scan(start=0, stop=1023, repeats = 3)
+def main():
+    diodeexperiment = DiodeExperiment()
+    voltages, currents, v_errors, c_errors = diodeexperiment.scan(start=0, stop=1023, repeats = 3)
 
-plt.figure()
-plt.errorbar(voltages, currents, xerr=v_errors, yerr=c_errors, fmt='o', ecolor='r')
-plt.xlabel('Voltage (V)')
-plt.ylabel('Current (A)')
-plt.show()
+    plt.figure()
+    plt.errorbar(voltages, currents, xerr=v_errors, yerr=c_errors, fmt='o', ecolor='r')
+    plt.xlabel('Voltage (V)')
+    plt.ylabel('Current (A)')
+    plt.show()
+
+main()
