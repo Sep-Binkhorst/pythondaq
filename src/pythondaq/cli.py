c2fd1e51c10ab35a7fb7fed2335838a6d639db66
Sep-Binkhorst <sepbinkhorst@gmail.com>
2024-11-19 10:12:26 +0100

    Group commands added.
    The functions list and scan are now able to be called. For scan, there is 1 argument for the range, and 3 options for where to start measuring, how many times you'd like to repeat and whether you want to make a csv file for the measurements.

--- src/pythondaq/cli.py
+++ src/pythondaq/cli.py
@@ -1,17 +1,48 @@
 from pythondaq.arduino_device import list_resources
+from pythondaq.diode_experiment import DiodeExperiment
 import click
+import csv
 
 @click.group()
 def cmd_group():
     pass
 
 
-@cmd_group.command()
+@cmd_group.command("list")
+
+def list():
+    print(f"Work in progress, list devices")
+    print(list_resources())
+
+    
+@cmd_group.command("scan")
 @click.option(
-    "--list",
-    help='.',
+    "--start",
+    default=0,
     show_default=True
 )
 
-def list():
-    print(list_resources())
+@click.argument("stop")
+
+@click.option(
+    "--repeats",
+    default=1,
+    show_default=True
+)
+
+@click.option(
+    "--output",
+)
+
+def scan(start, stop, repeats, output):
+    print(f"Work in progress, scan LED")
+    diodeexperiment = DiodeExperiment()
+    voltages, currents, v_errors, c_errors = diodeexperiment.scan(int(start), int(stop), int(repeats))
+    print(voltages)
+    print(currents)
+
+    with open(f'{output}.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Voltages', 'Currents'])
+        for voltage, current in zip(voltages, currents):
+            writer.writerow([voltage, current])
